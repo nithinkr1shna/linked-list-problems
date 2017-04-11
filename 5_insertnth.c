68780d32ced552feb4fff0601579ea4f0cccea28
nitkna <nithin.krishna@outlook.com>
2017-04-11 16:16:35 +0530

    insert element to nth position

---
+++ 5_insertnth.c
@@ -0,0 +1,139 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+struct node{
+
+  int data;
+  struct node* next;
+};
+
+
+
+void displayll(struct node *head);
+void Push(struct node** headRef, int newData); 
+void InsertNthTest(void);
+void InsertNth(struct node** headRef, int index, int num);
+int Length(struct node** head);
+struct node* BuildOneTwoThree();
+
+struct node *head = NULL; // declared globally
+
+int main(){
+  
+  InsertNthTest();
+  
+}
+
+// displays the elements in the Linked list
+void displayll(struct node* head){
+  if(Length(&head)==0)
+    printf("empty\n");
+  else{
+    struct node *current;
+    for(current =head;current != NULL; current = current->next)
+      printf("%d",current->data);
+    printf("\n");
+  }
+}
+
+// call the InsertNth function which which adds an elemnt to the specified index position the List
+void InsertNthTest(){
+
+  struct node* myList =BuildOneTwoThree();
+  printf("The created Linked list contains ");
+  displayll(head);
+  InsertNth(&head,0,6);
+  printf("The modified linked List\n");
+  displayll(head);
+  
+}
+
+// function responsible for building the Linked List {123}
+struct node* BuildOneTwoThree(){
+
+  int i;
+  for(i=1;i<4;i++)
+    Push(&head,i);
+  return(head);
+}
+
+// This Push function adds elements of int type to the end of the List.
+
+void Push(struct node** headRef, int newData){
+  struct node *newNode;
+  struct node *current= *headRef;
+  newNode = (struct node*)malloc(sizeof(struct node));
+  if(newNode == NULL){ // checks wheather new node is allocated
+    printf("cant allocate\n");
+    exit(0);
+  }
+  newNode->data = newData;
+  newNode->next = NULL;
+  
+  if(head == NULL){ // first ,checks if head is null
+     head = newNode;
+     
+  }else{
+    
+     while(current->next != NULL)
+       current = current->next;
+
+     current->next = newNode; //creates link 
+    
+  }
+}
+
+
+
+// returns the length of the Linked List.
+int Length(struct node** headRef){
+  if(*headRef == NULL)
+    return(0);
+  struct node* current = *headRef;
+  int len=0;
+  for(;current->next != NULL ;current = current->next)
+    len++;
+  return(len);
+}
+
+void InsertNth(struct node** headRef, int index, int num){
+
+  struct node* current = *headRef,*previous,*temp,*newNode;
+  int len=Length(&current)+1,link_count=-1;
+  
+  /* if(len+1 < index ){
+
+    printf("Insertion not possible, index out of range\nExiting.....\n");
+    exit(0);
+    }*/
+  printf("len %d index %d\n",len,index);
+  
+  if(len== index){
+    Push(&head,num);
+  }else if(index ==0){
+    temp = current;
+    newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = num;
+    newNode->next = temp->next;
+    head = newNode;
+    
+    
+  }else{
+
+    for(;current->next;current= current->next){
+
+      link_count++;
+      printf("%d\t\n",link_count);
+      if(link_count==index){
+	break;
+      }
+      previous = current;
+    }
+    newNode = (struct node*)malloc(sizeof(struct node));
+    newNode->data = num;
+    newNode->next = current;
+    previous->next = newNode;
+    
+  }
+  
+}
